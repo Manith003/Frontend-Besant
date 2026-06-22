ed127956204e1ce658b83068f844f8cd9bcdc513
Manith003 <manithkumar8190@gmail.com>
2026-06-22 11:50:36 +0530

    js day1

---
+++ js day1/script.js
@@ -0,0 +1,17 @@
+
+// three types printing values => console - alert - window.alert
+
+
+let name = "hello"
+let dob = "12/12/12"
+let phone= 1234567890
+const aadharnumber = 98765432112345
+let address = "qwertyuiopasdfghjklxcvbnm,"
+let photo = "image"
+
+console.log(name)
+console.log(dob)
+console.log(phone)
+console.log(aadharnumber)
+console.log(address)
+console.log(photo)
