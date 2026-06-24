0a591ea146b60ef711d3f6f84a183c85b169e616
Manith003 <manithkumar8190@gmail.com>
2026-06-24 11:59:29 +0530

    day2 datatypes

---
+++ js day2 datatypes/script.js
@@ -0,0 +1,12 @@
+const details = {
+    name: "Manith",
+    age: 21,
+    college: "loyola",
+    cgpa: 9.04,
+    project: "portfolio website"
+}
+
+console.log(`Hi, I'm ${details.name}. I'm ${details.age} years old and currently pursuing my BCA at ${details.college} College. I have a CGPA of ${details.cgpa}. I'm passionate about web development and recently built my own ${details.project} as a project.`)
+
+
+
