87f367a5ae06301088230b6c2004be71a7c950e1
Manith003 <manithkumar8190@gmail.com>
2026-06-25 11:50:36 +0530

    datatypes

---
+++ js day3 type convertion/script.js
@@ -0,0 +1,14 @@
+// binary conversion
+
+// (9) --> 10 ----> this is decimal
+// (1001) --> 2 ----> this is binary
+
+
+// let name = '123'
+// console.log(typeof(name))
+// console.log(Number(name))
+
+// --------->
+
+let a =123.123
+console.log(a)
