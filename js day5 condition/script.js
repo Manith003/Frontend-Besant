a1f5355593af55d64a763ed3e526e6d587c51bff
Manith003 <manithkumar8190@gmail.com>
2026-06-30 11:35:25 +0530

    day5 conditions

---
+++ js day5 condition/script.js
@@ -0,0 +1,65 @@
+// let num = +prompt("Enter a number from 1-7")
+
+// switch (num){
+//     case 1 : 
+//     console.log("one")
+//     break;
+//     case 2 : 
+//     console.log("two")
+//     break;
+//     case 3 : 
+//     console.log("three")
+//     break;
+//     case 4 : 
+//     console.log("four")
+//     break;
+//     case 5 : 
+//     console.log("five")
+//     break;
+//     case 6 : 
+//     console.log("six")
+//     break;
+//     case 7 : 
+//     console.log("seven")
+//     break;
+//     default : 
+//     console.log("make sure you entered 1-7")
+// }
+
+// let num = +prompt("Enter a number")
+
+// if (num < 0) {
+//     console.log("negative ")
+// } 
+// else if (num > 0) {
+//     console.log("positive")
+// }
+// else {
+//     console.log("zero")
+// }
+
+// let a = +prompt("enter the value for a: ")
+// let b = +prompt("enter the value for b: ")
+
+// if(a>b){
+//     console.log(`value of a is ${a} is greater`)
+// }else{
+//     console.log(`value of b is ${b} is greater`)
+
+// }
+
+// let num = +prompt("enter a number : ")
+
+// if(num%3 === 0 && num%5 === 0){
+//     console.log(`${num} is divisible by both`)
+// }
+// else{
+//     console.log(`${num} is NOT divisible by both`)
+// }
+
+
+// function numbers(a,b,c,...nums){
+//     console.log(nums)
+// }
+
+// numbers(1,2,3,4,5,6,7,8,9,0)
